9116e80b05dc43c7393be1dc507a4543115237bf
MasterOfPies <zhenghan.ren@gmail.com>
2024-11-11 18:42:06 +1100

    Working Max number of infected and Severely infected

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,20 +15,20 @@ using Design_Project
     γ_s = 1/14                        # Recovery rate for severe cases
     ps = 0.2                          # Proportion of severe cases
     σ = 1/30                          # Resusceptibility rate
-    ε = 0.3                           # Intervention efficacy
-    ϕ = 0.8                           # Proportion that uses intervention
+    ε = 0                           # Intervention efficacy
+    ϕ = 0                           # Proportion that uses intervention
     intervention_day = 30             # Day intervention starts
     params_fixed = (c, γ, γ_s, ps, σ, ε, ϕ, intervention_day) # Tuple of fixed parameters
 
-    # **Initial Conditions: S(0), I(0), Is(0), R(0)**
+    # Initial Conditions: S(0), I(0), Is(0), R(0)
     S0 = 5999
     I0 = 1
     Is0 = 0
     R0 = 0
     initial_conditions = [S0, I0, Is0, R0]
 
-    # **Time Span for the Simulation (in days)**
-    tspan = (0, 80)  # Simulate for 80 days
+    # Time Span for the Simulation (in days)
+    tspan = (0, 80)
 
     # Town 1
     # Infected people data (after day 30 intervention is added)
@@ -40,7 +40,7 @@ using Design_Project
                         337, 473, 673, 653, 523, 602, 551, 686, 556, 600]
     
     # Severely infected people data
-    days_severe = 22:80
+    days_severe = 21:80
     Sinfected_people = [0, 0, 1, 2, 5, 5, 5, 2, 9, 4, 22, 0, 15, 48, 38, 57, 9, 18, 20, 
                         0, 41, 15, 35, 36, 27, 38, 24, 40, 34, 57, 18, 29, 63, 66, 119, 
                         76, 95, 28, 109, 136, 119, 104, 121, 93, 147, 129, 130, 161, 133, 
@@ -56,26 +56,31 @@ using Design_Project
                             1480]
 
     # Severely infected people data
-    T2_days_severe = 27:80
+    T2_days_severe = 21:80
     T2_Sinfected_people = [3, 3, 4, 7, 3, 8, 7, 5, 9, 13, 15, 3, 20, 13, 11, 20, 16, 11, 15, 18, 27, 
                             24, 28, 36, 41, 35, 41, 55, 63, 66, 72, 80, 90, 104, 109, 115, 127, 135, 
                             147, 162, 163, 186, 194, 200, 216, 223, 241, 249, 258, 275, 277, 299, 302, 300]
 
-    # Define a Range of β Values
+    # Best beta value of infected people
+    # Define a range of β values
     beta_values = range(0.03, 0.038, length=20)
 
-    # Compute Errors for Each β in the Range
+    # Compute errors for each β in the range
     errors = [compute_error(β, params_fixed, initial_conditions, tspan, days_infected, infected_people) for β in beta_values]
 
-    # Plot Error as a Function of β to Justify Selection
+    # Plot error as a function of β to fustify selection
     p_error = plot(beta_values, errors, xlabel="β", ylabel="Sum of Squared Errors", 
                    title="Error vs β", lw=2)
     display(p_error)
 
-    # Find the β Value That Minimizes the Error
+    # Find the β value that minimizes the error
     min_error, min_index = findmin(errors)
     best_beta = beta_values[min_index]
-    println("Best β value: ", best_beta, " with minimum error: ", min_error)
+
+    # Compute error for the severely infected people
+    Severe_error = [compute_error(best_beta, params_fixed, initial_conditions, tspan, days_severe, Sinfected_people)]
+
+    println("Best β value: ", best_beta, " with infected error: ", min_error, " with severely infected error: ", Severe_error)
 
     # Solve the Model Again with the Best β
     β = best_beta
@@ -96,10 +101,19 @@ using Design_Project
     T1_Infected_Data_Plot = scatter!(days_infected, infected_people, label="Infected People Data")
     display(T1_Infected_Data_Plot)
 
+    #Plot just the severely infected people
+    T1_SInfected_Data_Plot = plot(sol, vars=(3), label="Severely Infected", xlabel="Days", ylabel="Population", title="SIRS Model - Severely Infected", lw=2)
+    T1_SInfected_Data_Plot = scatter!(days_severe, Sinfected_people, label="Severely Infected People Data")
+    display(T1_SInfected_Data_Plot)
+
     # Extract the infected population (second variable in the solution)
-    infected_population = sol[2, :]  # This accesses the second variable (I) at all time points
+    infected_population = sol[2, :]
 
     # Find the maximum number of infected people
     max_infected = maximum(infected_population)
     println("Maximum number of infected people: ", max_infected)
+
+    Sinfected_population = sol[3, :]
+    Smax_infected = maximum(Sinfected_population)
+    println("Maximum number of Severely infected people: ", Smax_infected)
 end
